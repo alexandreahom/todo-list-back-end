e93033605b7da6acfce01dc96c89f4a0e5dcd7fb
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-26 11:24:09 -0400

    half way

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,17 +2,27 @@ import { Todo } from '../models/todo.js'
 import { Profile } from '../models/profile.js'
 
 export function create (req,res){
+  // console.log(re.)
+  req.body.owner = req.params.id
   Todo.create(req.body)
   .then(todo =>{
     Profile.findById(req.params.id)
-    .populate('todos')
     .then(profile =>{
-      console.log(profile)
+      // console.log(profile)
       profile.todos.push(todo)
       profile.save()
-      .then(savedTodo=>{
-        console.log(savedTodo)
-        res.json(savedTodo)
+      .then(savvedProfile =>{
+        Profile.findById(req.params.id)
+        .populate({path:'todos',
+          populate:{
+            path:'owner',
+            model:'Profile'
+          }
+        })
+        .then(savedTodo=>{
+          console.log(savedTodo)
+          res.json(savedTodo)
+        })
       })
     })
   })
@@ -38,18 +48,45 @@ export function show(req, res) {
     res.json(todo)
   })
 }
-export function update(req, res) {
+// export function update(req, res) {
+//   console.log(req.params.id,"TEST")
+//   Todo.findById(req.params.id)
+//   .then(todo => {
+//     // console.log(req.user.profile,"#!$@%")
+//     if(todo.owner.equals(req.params.id)){
+//       todo = req.body.todo
+//       todo.save()
+//       .then(()=>{
+//         return res.json(todo)
+//       })
+//     }else{
+//       return res.status(500).json({err:'Not Allowed'})
+//     }
+//     res.json(todo)
+//   })
+// }
+export function deleted(req,res) {
+  console.log(req.params.id)
   Todo.findById(req.params.id)
   .then(todo => {
-    if(todo.author.equals(req.user.profile)){
-      todo = req.body.todo
-      todo.save()
-      .then(()=>{
-        return res.json(todo)
+    Profile.findById(req.user.profile)
+    .then(profile => {
+      profile.todos.remove(todo)
+      profile.save()
+      .then(savedProfile =>{
+        Profile.findById(req.user.profile)
+        .populate({
+          path: 'todos',
+          populate: {
+            path: 'owner',
+            model: 'Profile'
+          }
+        })
+        .then(populatedProfile => {
+          console.log(populatedProfile)
+          res.json(populatedProfile)
+        })
       })
-    }else{
-      return res.status(500).json({err:'Not Allowed'})
-    }
-    res.json(todo)
+    })  
   })
 }
